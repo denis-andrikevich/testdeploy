f47b74d16c81f50f8254a7d846a4bc62e96d723a
dandrikevych <dandrikevych@WS155.local>
2017-06-09 15:02:26 +0300

    first deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs/Observable";
+import { Store } from "@ngrx/store";
+
+interface AppState {
+  counter: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+
+
+export class AppComponent implements OnInit {
+  private counter: Observable<number>;
+
+  constructor(private store: Store<AppState>){
+    this.counter = store.select('counter');
+  }
+
+  increment(){
+    this.store.dispatch({type: 'INCREMENT'});
+  }
+
+  decrement(){
+    this.store.dispatch({type: 'DECREMENT'});
+  }
+
+
+
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { counterReducer } from "app/reducers/counter";
+import { StoreModule } from "@ngrx/store";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.provideStore({ counter: counterReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
